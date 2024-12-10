ec50a30e32a6ffc0b71252cfdd25b86ad3622ff7
AobaIwaki123 <aiwaki@gmail.com>
2024-12-10 16:33:33 +0900

    add fizzbuzz when

--- src/main/kotlin/fizzbuzz_when/Main.kt
+++ src/main/kotlin/fizzbuzz_when/Main.kt
@@ -1,3 +1,25 @@
 package fizzbuzz_when
 
-fun FizzBuzz(i: Int) {}
+fun fizzBuzz(i: Int): String {
+    return when {
+        i % 15 == 0 -> {
+            "FizzBuzz"
+        }
+        i % 3 == 0 -> {
+            "Fizz"
+        }
+        i % 5 == 0 -> {
+            "Buzz"
+        }
+        else -> {
+            "$i"
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val n: Int = 20
+    for (i in 1..n) {
+        println(fizzBuzz(i))
+    }
+}

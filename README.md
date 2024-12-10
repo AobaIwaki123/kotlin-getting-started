8e7914953172af646287f677a9025f6b5383ff09
AobaIwaki123 <aiwaki@gmail.com>
2024-12-10 16:17:43 +0900

    add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kotlin入門
+
+## Ref
+
+- https://zenn.dev/msksgm/books/implementing-server-side-kotlin-development
--- src/main/kotlin/fizzbuzz_if/Main.kt
+++ src/main/kotlin/fizzbuzz_if/Main.kt
@@ -1,7 +1,7 @@
 package fizzbuzz_if
 
 fun fizzBuzz(i: Int): String {
-    return  if( i % 15 == 0 ){
+    return if( i % 15 == 0 ){
         "FizzBuzz"
     } else if (i % 3 == 0 ){
         "Fizz"
